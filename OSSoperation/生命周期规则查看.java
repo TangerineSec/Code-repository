792bc43407d31f7f60a90b5ff2f24e91e522a0b9
TangerineSec <93525464+TangerineSec@users.noreply.github.com>
2025-05-07 14:22:23 +0800

    Create 生命周期规则查看.java

---
+++ OSSoperation/生命周期规则查看.java
@@ -0,0 +1,7 @@
+import com.aliyun.oss.ClientException;
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.common.auth.*;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.OSSException;
+import com.aliyun.oss.model.LifecycleRule;
+import java.util.List;
