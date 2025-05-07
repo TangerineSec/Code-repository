8727922c1c5f6cdb14e6310a725293352fb04339
TangerineSec <93525464+TangerineSec@users.noreply.github.com>
2025-05-07 14:19:49 +0800

    Update uploadfile.java

--- OSSoperation/uploadfile.java
+++ OSSoperation/uploadfile.java
@@ -1,3 +1,10 @@
+import com.aliyun.oss.*;
+import com.aliyun.oss.common.auth.*;
+import com.aliyun.oss.model.Bucket;
+import java.io.ByteArrayInputStream;
+import java.util.List;
+
+
             String objectName = "exampledir/exampleobject.txt";
             String content = "Hello OSS";
             String bucketName = "xtzhdd";
