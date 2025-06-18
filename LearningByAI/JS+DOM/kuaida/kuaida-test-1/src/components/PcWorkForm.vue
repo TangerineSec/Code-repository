e3a0f0c46185069069fe21ed1964cd58e2085d3e
yu_x <yuxiaolgd@foxmail.com>
2025-06-18 16:15:52 +0800

    修改文件

---
+++ LearningByAI/JS+DOM/kuaida/kuaida-test-1/src/components/PcWorkForm.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="box">
+        <button @click="handleClick">点击我</button>
+        <p>{{ text }}</p>
+    </div>
+</template>
+<script setup>
+import { ref } from 'vue';
+
+const text = ref('Hello World');
+
+function handleClick() {
+    text.value = '你点击了按钮';
+}
+</script>
+
+<style scoped>
+.box {
+    border: 1px solid #333;
+    padding: 20px;
+}
+</style>
+
